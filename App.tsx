bf67320ca8c2a6cfcc6dbf925ac31a71155bb4ab
Mazhar Hussain <mazhartejani@gmail.com>
2023-02-18 14:16:53 +0100

    Add react-native-paper & react-native-vector-icons

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
-import { Text, View } from 'react-native'
 import React from 'react'
 import { Provider } from 'react-redux'
 import { store } from './src/redux-store/store'
-import Counter from './src/screens/counter'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Counter />
-    </View>
-  )
-}
+import Login from './src/screens/Login'
 
 const Stack = createNativeStackNavigator()
 
@@ -22,7 +12,7 @@ const App = () => {
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={Login} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from 'react-native'
 import React from 'react'
+import { Text, View } from 'react-native'
 
 const Login = () => {
   return (
     <View>
-      <Text>Login</Text>
+      <Text>Login Screen</Text>
     </View>
   )
 }
