828e717851bda7038014470a9286ec8561f26e77
Mazhar Hussain <mazhartejani@gmail.com>
2023-02-18 16:24:11 +0100

    Add Login Screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,12 +1,59 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, Image } from 'react-native'
+import { TextInput, Button } from 'react-native-paper'
 
-const Login = () => {
+const logo = require('../assets/img/logo.png')
+
+const LoginScreen: React.FC = () => {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleLogin = () => {
+    // handle login logic here
+  }
+
   return (
-    <View>
-      <Text>Login Screen</Text>
+    <View style={styles.container}>
+      <Image source={logo} style={styles.logo} resizeMode="contain" />
+      <TextInput
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        mode="outlined"
+        style={styles.input}
+      />
+      <TextInput
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        mode="outlined"
+        style={styles.input}
+      />
+      <Button mode="contained" onPress={handleLogin} style={styles.button}>
+        Login
+      </Button>
     </View>
   )
 }
 
-export default Login
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  logo: {
+    height: 100,
+    width: '100%',
+    marginBottom: 20,
+  },
+  input: {
+    marginVertical: 10,
+  },
+  button: {
+    marginVertical: 10,
+  },
+})
+
+export default LoginScreen
